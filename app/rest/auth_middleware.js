af25872b64125b1aef6b2be6e6531ee5cb264c2c
SilGru <silgru@gmx.de>
2015-12-07 19:44:11 +0100

    auth middleware added

---
+++ app/rest/auth_middleware.js
@@ -0,0 +1,37 @@
+//import packages
+var express  = require('express');
+var router   = express.Router();
+var path     = require('path');
+var appDir   = path.dirname(require.main.filename);
+var config   = require(appDir + '/config');
+var jwt      = require('jsonwebtoken');
+
+router.use(function(req, res, next) {
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+	// decode token
+	if (token) {
+		// verifies secret and checks exp
+    try {
+      var decoded = jwt.verify(token, config.secret);
+      req.decoded = decoded;
+      next();
+    } catch(err) {
+      return res.json({ success: false, message: 'Failed to authenticate token.' });
+    }
+
+	} else {
+
+		// if there is no token
+		// return an error
+		return res.status(403).send({
+			success: false,
+			message: 'No token provided.'
+		});
+
+	}
+
+});
+
+module.exports = router;
