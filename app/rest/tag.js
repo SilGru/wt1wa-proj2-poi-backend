e63cf9dedce7c673a0702c67daae1ccab04b2bf0
SilGru <silgru@gmx.de>
2015-12-07 20:17:50 +0100

    4715 tag post

--- app/rest/tag.js
+++ app/rest/tag.js
@@ -7,3 +7,31 @@ var appDir   = path.dirname(require.main.filename);
 
 //import model
 var Tag    = require(appDir + '/app/model/tag');
+
+router.post('/tag', function(req, res) {
+  var name = req.body.name;
+  var user = req.user;
+
+  if (!name || 0 === name.trim().size) {
+    res.json({
+      "success": "false",
+      "error": "empty name"
+    })
+  }
+
+  var tag = new Tag({
+    "name": name,
+    "user": user._id,
+    "active": true
+  });
+
+  tag.save(function(err) {
+    if (err) res.send(err);
+    res.json({
+      "success": "true",
+      "id": tag._id
+    })
+  });
+});
+
+module.exports = router;
