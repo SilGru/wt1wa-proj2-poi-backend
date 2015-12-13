0c0c2f69ab1a22036d2acf40af78e7aea6125d4c
SilGru <silgru@gmx.de>
2015-12-13 16:59:51 +0100

    4726 GET tags scaffold

--- app/rest/tag.js
+++ app/rest/tag.js
@@ -8,40 +8,9 @@ var appDir   = path.dirname(require.main.filename);
 //import model
 var Tag    = require(appDir + '/app/model/tag');
 
-router.post('/tag', function(req, res) {
-  var name = req.body.name;
-  var user = req.user;
-
-  if (!name || 0 === name.trim().size) {
-    res.json({
-      "success": "false",
-      "error": "empty name"
-    })
-  }
-
-  Tag.findOne({ "name": name }, function(err, tag) {
-    if (tag) {
-      res.json({
-        "success": "false",
-        "error": "tag exists",
-        "tagId": tag._id
-      })
-    }
-  })
-
-  var tag = new Tag({
-    "name": name,
-    "user": user._id,
-    "active": true
-  });
-
-  tag.save(function(err) {
-    if (err) res.send(err);
-    res.json({
-      "success": "true",
-      "id": tag._id
-    })
-  });
+router.get('/tags', function(req, res) {
+  var aIds = req.body.aIds;
+  
 });
 
 module.exports = router;
