1f36a77bdc94a5fd4b392d62cd6034eb547a7203
SilGru <silgru@gmx.de>
2015-12-21 16:41:03 +0100

    poi validate impl

--- app/rest/auth/poi.js
+++ app/rest/auth/poi.js
@@ -4,11 +4,50 @@ var router   = express.Router();
 var mongoose = require('mongoose');
 var path     = require('path');
 var appDir   = path.dirname(require.main.filename);
+var validator = require(appDir + 'app/util/validator');
 
 //import model
 var Poi    = require(appDir + '/app/model/poi');
 
+/**
+ * Create poi
+ */
 router.post('/poi', function(req, res) {
+  var name = req.body.name;
+  var description = req.body.description;
+  var lat = req.body.lat;
+  var lon = req.body.lon;
+  var user = req.user;
+
+  //check poi validity
+  if (!validator.validateString(name)) {
+    res.json({
+      "success": "false",
+      "error": "empty name"
+    })
+  }
+
+  if (!validator.validateString(decription)) {
+    res.json({
+      "success": "false",
+      "error": "empty description"
+    })
+  }
+
+  if (!validator.validateLon(lon)) {
+    res.json({
+      "success": "false",
+      "error": "lon invalid"
+    })
+  }
+
+  if (!validator.validateLon(lat)) {
+    res.json({
+      "success": "false",
+      "error": "lat invalid"
+    })
+  }
+
 });
 
 module.exports = router;
