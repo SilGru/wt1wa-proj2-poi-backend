894b6d12788db02df71fc98431c294474e11781e
SilGru <silgru@gmx.de>
2015-12-21 16:07:02 +0100

    4712 poi auth rest endpoint scaffold

---
+++ app/rest/auth/poi.js
@@ -0,0 +1,12 @@
+//import packages
+var express  = require('express');
+var router   = express.Router();
+var mongoose = require('mongoose');
+var path     = require('path');
+var appDir   = path.dirname(require.main.filename);
+
+//import model
+var Poi    = require(appDir + '/app/model/poi');
+
+
+module.exports = router;
