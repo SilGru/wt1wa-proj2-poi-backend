6322d685c69289c09162ee4fd4e5ba95a15729a8
SilGru <silgru@gmx.de>
2015-12-29 17:18:04 +0100

    4731 validate comment impl.

--- app/rest/auth/comment.js
+++ app/rest/auth/comment.js
@@ -4,11 +4,25 @@ var router   = express.Router();
 var mongoose = require('mongoose');
 var path     = require('path');
 var appDir   = path.dirname(require.main.filename);
+var validator = require(appDir + '/app/util/validator');
 
 //import model
 var Comment  = require(appDir + '/app/model/comment');
 
 router.post('/comment', function(req, res) {
+  var content = req.body.content;
+  var user = req.user;
+
+  //check comment validity
+  if (!validator.validateString(content)) {
+    res.json({
+      "success": "false",
+      "error": "empty content"
+    })
+  }
+
+  
+
 });
 
 module.exports = router;
