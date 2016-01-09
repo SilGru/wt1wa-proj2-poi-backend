4005d3c7731efeb2def9280322c48ed78a4186a9
SilGru <silgru@gmx.de>
2016-01-09 17:27:18 +0100

    restTestPoi impl.

---
+++ test/restTestPoi.js
@@ -0,0 +1,24 @@
+var request = require('supertest');
+
+describe('Pois', function() {
+
+  it('respond with json', function(done){
+    request("https://rocky-stream-2635.herokuapp.com/api")
+      .get('/pois')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'application/json')
+      .expect(200, done);
+  });
+
+  it('has to aquire a token for further requests', function(done){
+    request("https://rocky-stream-2635.herokuapp.com/api")
+      .post('/authenticate')
+      .send({ name: "Peter", password: "password"})
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        console.log(res.body);
+      })
+      .expect(200, done);
+  });
+
+});
