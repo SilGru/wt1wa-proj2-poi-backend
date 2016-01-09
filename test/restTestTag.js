d96bb1db250e5f0414f145e5edab4b1af9eaaeb0
SilGru <silgru@gmx.de>
2016-01-09 16:16:22 +0100

    more restTag testing

--- test/restTestTag.js
+++ test/restTestTag.js
@@ -1,12 +1,17 @@
 var request = require('supertest');
 
-describe('Account', function() {
+describe('Tags', function() {
   it('respond with json', function(done){
-    // the request-object is the supertest top level api
     request("https://rocky-stream-2635.herokuapp.com/api")
       .get('/tags')
       .set('Accept', 'application/json')
       .expect('Content-Type', 'application/json')
-      .expect(200, done); // note that we're passing the done as parameter to the expect
+      .expect(200, done);
+  });
+  it('respond with forbidden if no token is provided', function(done){
+    request("https://rocky-stream-2635.herokuapp.com/api")
+      .get('/tag')
+      .set('Accept', 'application/json')
+      .expect(403, done);
   });
 });
